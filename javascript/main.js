69666977db32568fc24947b05767298936bfc700
Tadeu <tadeulima14@gmail.com>
2023-12-07 03:21:35 -0800

    backup update

--- javascript/main.js
+++ javascript/main.js
@@ -2,6 +2,8 @@ const apiKey = '628334ae830de6763bbdd646a69b7964';
 let cityName = 'chicago';
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
 const searchBtn = document.querySelector('.searchbar button');
+const initialDefaultImagePath = 'images/default.png';
+document.querySelector('.weather-icon').setAttribute('src', initialDefaultImagePath);
 
 // Make the API request
 async function fetchWeatherData() {
@@ -11,11 +13,14 @@ async function fetchWeatherData() {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
     const data = await response.json();
+    cityTime = data.dt; 
     updateWeatherData(data);
   } catch (error) {
     console.error('Error fetching data:', error);
   }
 }
+window.addEventListener('load', fetchWeatherData);
+
 
 // Event listener for the search button
 if (searchBtn) {
@@ -34,12 +39,74 @@ function searchCity() {
   }
 }
 
+function formatTime(timestamp, timezoneOffset) {
+    const localTimestamp = timestamp + timezoneOffset;
+    const localDate = new Date(localTimestamp * 1000);
+    const isoTimeString = localDate.toISOString();
+  
+    // Extract the time part (HH:mm) from the ISO string
+    const isoTime = isoTimeString.slice(11, 16);
+  
+    return isoTime;
+  }
+
+
 // Function to update weather data on the page. locates html classes
 function updateWeatherData(data) {
   document.querySelector('.city').innerHTML = data.name;
   document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°F';
   document.querySelector('.humidity').innerHTML = data.main.humidity + ' %';
   document.querySelector('.wind').innerHTML = data.wind.speed + ' Kts/hr';
+  let timezoneOffset = data.timezone;
+  document.querySelector('.time').innerHTML = formatTime(data.dt, timezoneOffset);
+  document.querySelector('.weather-icon').innerHTML = data.weather.main;
+
+  const weatherMain = data.weather[0].main;
+  updateImage(weatherMain);
+
 }
 
-fetchWeatherData();
+async function fetchWeatherData() {
+    try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      cityTime = data.dt; 
+      updateWeatherData(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+function updateImage(weatherMain) {
+    const formattedWeatherMain = weatherMain.toLowerCase().replace(/\s/g, '-');
+    let imagePath;
+  
+    // Use if statements to determine the appropriate image path based on weather conditions
+    if (weatherMain === 'Rain') {
+      imagePath = 'images/rain.png';
+    } else if (weatherMain === 'Clear') {
+      imagePath = 'images/clear.png';
+    } else if (weatherMain === 'Clouds') {
+      imagePath = 'images/clouds.png';
+    } else if (weatherMain =='Snow') {
+        imagePath = 'images/snow.png';
+    } else if (weatherMain =='Mist') {
+        imagePath = 'images/mist.png';
+    } else if (weatherMain =='Fog') {
+        imagePath = 'images/fog.png';
+    } else if (weatherMain =='Haze') {
+        imagePath = 'images/haze.png';
+    }
+
+    else {
+      // Default image if no specific condition matches
+      imagePath = 'images/placeholder.png';
+    }
+  
+    document.querySelector('.weather-icon').setAttribute('src', imagePath);
+}
+
+
